43be568b4ea085470c3b8e483f65540f8b979996
ldm0805 <leonardodimaggio94@gmail.com>
2023-01-17 17:04:39 +0100

    aggiunta immagini

--- src/components/AppSingleCard.vue
+++ src/components/AppSingleCard.vue
@@ -16,7 +16,7 @@ export default {
 <template lang="">
 
 <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="">
+  <img class="card-img-top" :src="`https://www.themoviedb.org/t/p/w500/${(movie.poster_path)}`">
   <div class="card-body">
     <h5 class="card-title">Titolo del film: {{ movie.title }}</h5>
     <p class="card-text">Titolo Originale: {{ movie.original_title }}</p>
