e180ff6003388ea71e1208486a802e215e9fb33a
ldm0805 <leonardodimaggio94@gmail.com>
2023-01-17 19:48:51 +0100

    Aggiunta serie

--- src/components/AppSingleCard.vue
+++ src/components/AppSingleCard.vue
@@ -1,20 +1,20 @@
 <script>
 
 export default {
-    name: 'Card',
-    props: {
-        movie: Object,
-    },
+  name: 'Card',
+  props: {
+    movie: Object,
+  },
 }
 </script>
 
 <template lang="">
 
-<div class="card" style="width: 18rem;">
+<div class="card">
   <img class="card-img-top" :src="`https://www.themoviedb.org/t/p/w300/${(movie.poster_path)}`">
   <div class="card-body">
-    <h5 class="card-title">Titolo del film: {{ movie.title }}</h5>
-    <p class="card-text">Titolo Originale: {{ movie.original_title }}</p>
+    <h5 class="card-title">Titolo: {{ movie.title }} {{movie.name}}</h5>
+    <p class="card-text">Titolo Originale: {{ movie.original_title }} {{movie.name}}</p>
     <p class="card-text">Voto: {{ movie.vote_average }}</p>
     <p class="card-text">Lingua: {{ movie.original_language }}</p>
     <img :src="`https://www.countryflagicons.com/FLAT/64/${(movie.original_language).toUpperCase()}.png`">
--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { reactive } from 'vue';
 export const store = reactive({
     apiKeys: 'a8f3b6f9513218c50175fe4adfab3d55',
-    apiMovie: 'https://api.themoviedb.org/3/search/movie?api_key=a8f3b6f9513218c50175fe4adfab3d55&language=en-US&query=',
+    apiMovie: 'https://api.themoviedb.org/3/search/multi?api_key=a8f3b6f9513218c50175fe4adfab3d55&language=en-US&query=',
     flagApi: 'https://countryflagsapi.com/png/ae',
     movieArr: [],
 })
