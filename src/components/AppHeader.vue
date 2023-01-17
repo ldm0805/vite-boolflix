b15b37b1bb8743c4708e53da39f9a47e0673f261
ldm0805 <leonardodimaggio94@gmail.com>
2023-01-17 15:25:44 +0100

    push base

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+
+}
+</script>
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+@use '../styles/partials/mixins' as *;
+</style>
---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { reactive } from 'vue';
+export const store = reactive({
+    apiKeys: 'a8f3b6f9513218c50175fe4adfab3d55',
+    api: 'https://api.themoviedb.org/3/search/movie?api_key=a8f3b6f9513218c50175fe4adfab3d55&language=en-US&query=',
+    movieArr: [],
+
+
+})
