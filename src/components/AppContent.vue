d468657c29beb28ed694f8207656188881109e26
ldm0805 <leonardodimaggio94@gmail.com>
2023-01-19 09:36:45 +0100

    Aggiunta searchbar

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -17,16 +17,16 @@ export default {
 }
 </script>
 <template lang="">
-    <div v-if="store.loading">
-        <div class="d-flex justify-content-center">
-            <div class="loading animate__infinite animate__heartBeat_ ">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png" alt="">
+    <div class="container-all">
+        <div v-if="store.loading">
+            <div class="d-flex justify-content-center">
+                <div class="txt" contenteditable="true">Boolflix</div>
             </div>
         </div>
-    </div>
-    <div v-else>
-        <div class="container-card mt-5">
-            <AppSingleCard v-for="(item, index) in store.movieArr" :key="index" :details="item" v-model="change" @change="$emit('selection', change)"/>
+        <div v-else>
+            <div class="container-card mt-5">
+                <AppSingleCard v-for="(item, index) in store.movieArr" :key="index" :details="item" v-model="change" @change="$emit('selection', change)"/>
+            </div>
         </div>
     </div>
 </template>
@@ -36,19 +36,86 @@ export default {
     display: flex;
     overflow-x: auto;
     overflow-y: hidden;
-    margin-bottom: 2em;
 }
 
-img {
-    width: 80%;
+.container-all {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
 }
 
 .loading {
+
     width: 500px;
     display: flex;
     justify-content: center;
-    margin-top: 10em;
     animation: heartBeat infinite;
-    animation-duration: 2s;
+    animation-duration: 3s;
+
+    img {
+        width: 100%;
+    }
+}
+
+@function makelongshadow($length, $angle) {
+    $val: 0px 0px transparent;
+
+    @for $i from 1 through $length {
+        $val: #{$val},
+        #{$i}px #{$i * $angle}px rgb(99, 99, 99);
+    }
+
+    @return $val;
+}
+
+
+
+.txt {
+    text-align: center;
+    font-family: "Roboto";
+    font-size: 12vmin;
+    font-weight: 700;
+    animation: netflix_style 3.5s infinite;
+    outline: none;
+    white-space: nowrap;
+}
+
+@keyframes netflix_style {
+    0% {
+        text-shadow: makelongshadow(100, 1);
+        color: #868484;
+        transform: scale(1.5, 1.5);
+    }
+
+    10% {
+        text-shadow: makelongshadow(100, 1.5);
+        color: #f3f3f3;
+        transform: scale(1.5, 1.5);
+    }
+
+    15% {
+        color: #f3f3f3;
+    }
+
+    20% {
+        color: #e90418;
+        text-shadow: none;
+        transform: scale(1.1, 1.1);
+    }
+
+    75% {
+        opacity: 1;
+    }
+
+    80% {
+        opacity: 0;
+        color: #e90418;
+        transform: scale(0.85, 0.9);
+    }
+
+    100% {
+        opacity: 0;
+    }
 }
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,16 +4,67 @@ export default {
     components: {
         AppSearch,
     },
-
+    data() {
+        return {
+            activeItem: 0,
+            menu: [
+                {
+                    label: 'Home',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'Film',
+                    url: '#',
+                    active: true
+                },
+                {
+                    label: 'Serie Tv',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'Documentari',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'Nuovi e popolari',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'La mia lista',
+                    url: '#',
+                    active: false
+                },
+            ]
+        }
+    },
+    methods: {
+        selectedItem(index) {
+            this.activeItem = index
+        },
+    }
 }
 </script>
 <template lang="">
     <div class="search_cont">
-        <div class="left-search">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/720px-Netflix_2015_logo.svg.png" alt="netflix_logo">
+        <div class="left-search d-flex">
+            <h1>Boolflix</h1>
+            <ul>
+                <!-- For per la navbar + click sugli elementi per assegnare la classe active -->
+                <li v-for="(item, index) in menu" :key="index" @click="selectedItem(index)"
+                    :class="(activeItem === index) ? 'active' : ''">
+                    <a :href="item.url">
+                        {{ item.label }}
+                    </a>
+                </li>
+            </ul>
         </div>
-        <div class="right-search">
+        <div class="right-search d-flex">
             <AppSearch />
+            
         </div>
     </div>
 </template>
@@ -30,10 +81,15 @@ export default {
     align-items: center;
     background-color: black;
 
+    h1 {
+        color: red;
+    }
+
 
     .left-search,
     .right-search {
-        padding: 2em 2em;
+        padding: 1em 2em;
+        align-items: center;
 
         img {
             width: 30%;
@@ -42,5 +98,27 @@ export default {
 
 
 
+}
+
+ul {
+    display: flex;
+    margin: 2rem 1rem;
+    list-style-type: none;
+
+    .active {
+        border-bottom: 3px solid red;
+        padding-bottom: 1em;
+    }
+
+    li a {
+        padding: 1em;
+        font-size: 16px;
+        text-decoration: none;
+        transition: border 0.3s;
+        color: white;
+        font-size: 15px;
+        font-weight: 500;
+
+    }
 }
 </style>
